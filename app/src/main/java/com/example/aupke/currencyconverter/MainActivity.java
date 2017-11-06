25559aa6dcbcf526b10619ee12e1ed4ef90e9d69
Tobias <Aupke@hotmail.com>
2017-11-06 15:43:39 +0100

    Currency converting is working. Now working on Swap.button

--- app/src/main/java/com/example/aupke/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/aupke/currencyconverter/MainActivity.java
@@ -14,20 +14,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void convert(View view) throws NoSuchFieldException, IllegalAccessException {
         EditText editText = (EditText) findViewById(R.id.editText2);
-        Double dollarAmount = Double.parseDouble(editText.getText().toString());
-        TypedValue typedValue = new TypedValue();
+        Double readAmount = Double.parseDouble(editText.getText().toString());
 
         String spinnerString = getSpinner(R.id.toSpinner);
-        Resources resources = getResources();
-        float value = typedValue.getFloat();
-        resources.getValue(resources.getIdentifier(spinnerString, "dimen", MainActivity.this.getPackageName()), typedValue, true);
+        String spinnerString2 = getSpinner(R.id.spinner);
+        Double spinnerCurrencyFrom = getSpinnerPosition(R.id.spinner);
+        Double spinnerCurrencyTo = getSpinnerPosition(R.id.toSpinner);
+        //resources.getValue(resources.getIdentifier(spinnerString, "dimen", MainActivity.this.getPackageName()), typedValue, true);
+        //resources.getValue(spin);
 
 
-
-        Double euroConvert = dollarAmount*value;
+        Double euroConvert = readAmount*spinnerCurrencyTo/spinnerCurrencyFrom;
         TextView textView = (TextView) findViewById(R.id.editText);
-        textView.setText(euroConvert.toString() + "  Euro");
-        Log.i("Spinner from ", getSpinner(R.id.toSpinner));
+        textView.setText(euroConvert.toString());
+        Log.i("Spinner from ", spinnerString);
+        Log.i("Spinner from ", spinnerString2);
+        Log.i("Second spinner value: ", spinnerCurrencyFrom.toString());
     }
 
     public String getSpinner(int spinner){
@@ -35,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
         String text = toSpinner.getSelectedItem().toString();
         return text;
     }
+
+    public Double getSpinnerPosition(int spinner){
+        Spinner spinner1 = (Spinner) findViewById(spinner);
+        int spinnerPosition = spinner1.getSelectedItemPosition();
+        String[] currency_values = getResources().getStringArray(R.array.currencyInteger);
+        double sizeD = Double.valueOf(currency_values[spinnerPosition]);
+        return sizeD;
+    }
+
+    public void swap(View view){
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
