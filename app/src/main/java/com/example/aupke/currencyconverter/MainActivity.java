01aacb15d74db2b17102fc3d5e381a1e0eb3c5cd
mabdesaken <mathias.abdesaken@gmail.com>
2017-11-07 13:36:42 +0100

    Have made history button, and moving on to implementing it into a listview

--- app/src/main/java/com/example/aupke/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/aupke/currencyconverter/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.aupke.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import static java.lang.Math.round;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,5 +58,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button historyListViewButton = (Button)findViewById(R.id.historyButton);
+        historyListViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent switchToListView = new Intent(getApplicationContext(), HistoryActivity.class);
+                startActivity(switchToListView);
+            }
+        });
     }
 }
